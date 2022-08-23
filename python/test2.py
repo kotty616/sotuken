923863db59ae1dea5c3c2fcc4de0c9b1d09a7f7f
kotty616 <kotty.i375@gmail.com>
2022-08-24 01:29:08 +0900

    Add Serial

--- python/test2.py
+++ python/test2.py
@@ -1,15 +1,23 @@
-import requests, time, sys
+import requests, time, sys,serial
 from datetime import datetime
 
 url = 'http://127.0.0.1:5000/post'
+ser = serial.Serial("/dev/cu.usbmodem142101", 9600, timeout=1)
 
 def post(data):
     r = requests.post(url, data={"data": str(data)})
     time.sleep(3)
     print(r.status_code)
 
+def read_data():
+    datalist = []
+    for i in range(10):    
+        while True:
+            if ser.in_waiting > 0:
+                data = ser.read_all().decode().split('\r\n')
+                datalist.append(data)
+                break
+    return datalist
+
 if __name__ == '__main__':
-    while 1:
-        data = [sys.argv[1],datetime.now().strftime('%H:%M:%S')]
-        post(data)
-        time.sleep(3)
+    print(read_data())
