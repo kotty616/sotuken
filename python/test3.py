0b76dfd9dce2e897670878bb2ca3a7da6f4ea8ef
kotty616 <kotty.i375@gmail.com>
2022-09-15 17:34:57 +0900

    hosei

--- python/test3.py
+++ python/test3.py
@@ -1,7 +1,7 @@
 import requests, time, serial
 from datetime import datetime
 
-url = 'http://127.0.0.1:5000/post'
+url = 'http://localhost:5000/post'
 ser = serial.Serial("/dev/ttyACM0", 9600, timeout=1)
 
 def post(data):
@@ -15,12 +15,15 @@ def read_data():
         data = ser.readline().decode().replace('\r\n','')
         if data != '':
             vol = round(int(data)/ 1023 *5, 7)
-            # 距離*電圧=30
-            dis = round(28/vol, 7)
+            hosei = 28
+            # 距離*電圧=30(2.6V*10cm=26, 1.0V*30cm=30)
+            dis = round(hosei/vol, 7)
             datalist.append(dis)
         if len(datalist) > 10:
             break
     return datalist
 
 if __name__ == '__main__':
+    data =read_data()
     print(read_data())
+    post(data)
