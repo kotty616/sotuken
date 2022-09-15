2760b6b95f0958e74cdc086dee22c0023f259392
kotty616 <kotty.i375@gmail.com>
2022-09-15 15:10:00 +0900

    float

--- python/test2.py
+++ python/test2.py
@@ -2,7 +2,7 @@ import requests, time, sys,serial
 from datetime import datetime
 
 url = 'http://127.0.0.1:5000/post'
-ser = serial.Serial("/dev/cu.usbmodem142101", 9600, timeout=1)
+ser = serial.Serial("/dev/ttyACM0", 9600, timeout=1)
 
 def post(data):
     r = requests.post(url, data={"data": str(data)})
@@ -11,12 +11,12 @@ def post(data):
 
 def read_data():
     datalist = []
-    for i in range(10):    
-        while True:
-            if ser.in_waiting > 0:
-                data = ser.read_all().decode().split('\r\n')
-                datalist.append(data)
-                break
+    while True:   
+        data = ser.readline().decode().replace('\r\n','')
+        if data != '':
+            datalist.append(int(data)/ 1023 *5)
+        if len(datalist) > 10:
+            break
     return datalist
 
 if __name__ == '__main__':
