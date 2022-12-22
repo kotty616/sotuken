684bd14ac1cf929295481264d64e123ffff36d97
kotty616 <kotty.i375@gmail.com>
2022-12-22 14:25:14 +0900

    gui

--- python/senser_json.py
+++ python/senser_json.py
@@ -8,9 +8,23 @@ url = 'http://127.0.0.0:5000/tmp'
 def dataset(id):
     datalist = {"id":id}
     print("start")
-    while True:
-        if len(datalist) > 15: 
-            break
+    try:
+        while True:
+            if len(datalist) > 15: 
+                break
+            data = ser.readline().decode().replace('\r\n','')
+            if data != '':
+                vol = round(int(data,16)/ 1023 *5, 7)
+                hosei = 28
+                if vol > 0:
+                    # 距離*電圧=30 (2.6V*10cm=26, 1.0V*30cm=30)
+                    dis = round(hosei/vol, 7)
+                    dt_now = datetime.now()
+                    data = {"dis":dis, "time":dt_now.strftime("%H:%M:%S.%f")}
+                    print(data)
+                    datalist[f"data{len(datalist)}"] = data
+                    time.sleep(.5)
+    except KeyboardInterrupt as e:
         data = ser.readline().decode().replace('\r\n','')
         if data != '':
             vol = round(int(data,16)/ 1023 *5, 7)
@@ -22,8 +36,8 @@ def dataset(id):
                 data = {"dis":dis, "time":dt_now.strftime("%H:%M:%S.%f")}
                 print(data)
                 datalist[f"data{len(datalist)}"] = data
-                time.sleep(.5)
-    # print(datalist)
+        return json.dumps(datalist)
+        # print(datalist)
     return json.dumps(datalist)
 
 def post(data):
